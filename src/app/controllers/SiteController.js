6d9c7db9e80154e0eaae10208bd091fc79749a2b
An <nguyenvanan7580@gmail.com>
2024-10-14 08:54:47 +0700

    update process

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -3,7 +3,7 @@ const User = require("../models/userModel");
 class SiteController{
     //[GET] /
     index(req, res, next){
-        res.render('dangkidetai', {
+        res.render('RegisterTopic', {
             showHeaderFooter: true,
             showNav: true,
             title: 'Home',
