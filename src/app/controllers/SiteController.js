e11461f0342d50e35b1f3baa62711ce2e5d6508d
An <nguyenvanan7580@gmail.com>
2024-10-15 21:48:12 +0700

    save change

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -3,7 +3,7 @@ const User = require("../models/userModel");
 class SiteController{
     //[GET] /
     index(req, res, next){
-        res.render('home', {
+        res.render('student/AccountInfo', {
             showHeaderFooter: true,
             showNav: true,
             title: 'Home',
