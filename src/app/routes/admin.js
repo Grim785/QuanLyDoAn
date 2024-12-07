3ce26ed285bd4e3295f3d91c0ee901a80ed2b492
NguyenPhat <nguyenphat@gmail.com>
2024-12-07 15:10:12 +0700

    sualoi

--- src/app/routes/admin.js
+++ src/app/routes/admin.js
@@ -6,8 +6,6 @@ const controllers = require('../controllers');
 //[GET] /
 router.get('/dashboard', controllers.admin.dashboard);
 //[GET] /admin/TopicList
-router.get('/TopicList', controllers.admin.topicList);
-
 router.get('/TopicList', controllers.admin.TopicList);
 
 router.get('/AccountManagement',controllers.admin.AccountManagement);
