cbfc5493ac07eb9f534c9449537638cd4d520684
Grim785 <129041656+Grim785@users.noreply.github.com>
2025-05-01 21:59:03 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -5,10 +5,8 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  {
-      host: process.env.DB_HOST,
-      dialect: 'mysql',
-  }
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
 });
 
 connection.connect((err) => {
