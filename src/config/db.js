379f75f90159cc85dcba003ad19a31eda61fae08
Grim785 <129041656+Grim785@users.noreply.github.com>
2025-05-01 23:21:51 +0700

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();  // Tải các biến môi trường từ .env
 const { Sequelize } = require('sequelize');
 
 // Kết nối với cơ sở dữ liệu MySQL bằng cách sử dụng các biến môi trường
-const sequelize = new Sequelize(process.env.MYSQL_ADDON_URI, {
+const sequelize = new Sequelize(process.env.CONNECT_URI, {
   dialect: 'mysql',
   logging: false,  // Tắt logging nếu không cần thiết
 });
